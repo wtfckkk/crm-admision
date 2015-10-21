3736a3644abe87be998ce631fb36b674634f7771
wtfckkk <tcornejo01@gmail.com>
2015-10-21 20:46:58 -0300

    commit centosla

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Refresh" content="0;url=http://172.25.6.65/crm/public/"></meta>
+    <meta http-equiv="Refresh" content="0;url=http://200.73.112.119/crm/public/"></meta>
 
 </head>
 
